aea775b33b1884060167d86a15beb8fbdf901d4c
jiashunx <wenjiashun@163.com>
2024-05-09 12:17:54 +0800

    :passport_control: eureka server增加安全认证配置

---
+++ src/main/java/io/github/jiashunx/eureka/server/config/SecurityConfig.java
@@ -0,0 +1,18 @@
+package io.github.jiashunx.eureka.server.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * 关闭spring-boot-starter-security的CSRF校验
+ * @author jiashunx
+ */
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        super.configure(http);
+        http.csrf().disable();
+    }
+}
